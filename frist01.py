43e64c6ba646ee644a264f403cdaad0e0ca15acc
164150-droid <164150@wsk.ac.th>
2026-06-16 04:14:10 +0000

    frist01.py

---
+++ frist01.py
@@ -0,0 +1,6 @@
+print("โปรแกรมคำนวณหาพื้นที่ของสามเหลี่ยม")
+h = float(input("enter height: "))
+b = float(input("enter base: "))
+area = 0.5 * b * h
+print("พื้นที่ของสามเหลี่ยม =", area)
+print("จัดทำโดย นายศุภณัฐ สุขสว่าง เลขที่7")
